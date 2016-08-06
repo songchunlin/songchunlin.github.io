764870f1fd8b8521ddeb39865baf72b92082c809
songchunlin <sclyeah@gmail.com>
2016-08-06 19:11:52 +0800

    Update jquery.tagcloud.js

--- media/js/jquery.tagcloud.js
+++ media/js/jquery.tagcloud.js
@@ -24,7 +24,7 @@
         $(this).css({"font-size": opts.size.start + (weighting * fontIncr) + opts.size.unit});
       }
       if (opts.color) {
-        $(this).css({"color": tagColor(weighting, opts.color, colorIncr)});
+        $(this).css({"color": tagColor(opts.color, colorIncr, weighting)});
       }
     });
   };
